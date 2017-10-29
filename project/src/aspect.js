6abd3387301325402b4a46cb047cc4552a75ea9e
Kibo <tomasjurman@gmail.com>
2017-10-29 15:43:35 +0100

    v0.3.10

--- project/src/aspect.js
+++ project/src/aspect.js
@@ -84,10 +84,41 @@
 	/**
 	 * Transit aspects
 	 *
+	 * @param {Object} points - transiting points; {"Sun":[0, 1], "Uranus":[90, -1], "NAME":[ANGLE, SPEED]}; 
 	 * @return {Array<Object>} [{"aspect":"conjunction", "point":"Sun", "toPoint":"Moon", "precision":0.5}]]
 	 */
-	astrology.AspectCalculator.prototype.transit = function( transitData ){		
-		return [{a:1}];
+	astrology.AspectCalculator.prototype.transit = function( points ){	
+		if(!points){
+			return []; 
+		}
+		
+		var aspects = [];
+		
+		for (var point in points) {
+ 		   if (points.hasOwnProperty( point )) { 		   	
+ 		   		for (var toPoint in this.toPoints) {
+ 		   			if (this.toPoints.hasOwnProperty( toPoint )) {
+ 		   		
+ 		   				for(var aspect in this.settings.aspects){ 		   				
+	 		   				if(hasAspect( points[point][0], this.toPoints[toPoint][0], this.settings.aspects[aspect])){	 
+	 		   					
+	 		   					var precision = calcPrecision(points[point][0], this.toPoints[toPoint][0], this.settings.aspects[aspect]["degree"]);	 		   						 		   							   					
+	 		   					aspects.push(
+	 		   								{
+	 		   								"name":aspect, 
+	 		   								"precision":calcDirection( this.settings.aspects[aspect]["degree"], this.toPoints[toPoint][0], points[point][0], points[point][1]) * precision,
+	 		   								"point":point, 
+	 		   								"toPoint":toPoint
+	 		   								}
+	 		   							)
+	 		   				}	 		   				
+	 		   			} 		   		 		   		 		   	
+ 		   			}
+ 		   		} 		   	
+ 		   } 		
+ 		}
+ 		 		   						
+		return aspects;
 	};
 	
 	/*
@@ -122,8 +153,61 @@
  	* @param {double} aspectDegree;
 	 */
 	function calcPrecision(point, toPoint, aspect){
-		var gap = Math.abs( point - toPoint );		
+		var gap = Math.abs( point - toPoint );
+		
+		if( gap > astrology.utils.radiansToDegree( Math.PI)){
+			gap = astrology.utils.radiansToDegree( 2 * Math.PI) - gap;
+		}			
 		return Math.abs( gap - aspect);
+	}
+	
+	/*
+	 * Calculate direction of aspect
+	 * whether the transiting planet is approaching or is falling
+	 * @private
+	 * 
+	 * //TODO
+	 * This method is tested, and for tests gives the right results. 
+	 * But the code is totally unclear. It needs to be rewritten.
+	 * *
+	 * @param {Integer} aspect - aspect angle, for example 90
+	 * @param {double} toPoint - angle of radix standing point
+	 * @param {double} point - angle of transiting planet
+	 * @param {double | undefined} speed - speed of transiting planet
+	 * 
+	 * @return {+1 | -1}
+	 * -1 transit planet is approaching the radix point.
+	 * +1 transit planet is falling the radix point.
+	 */
+	function calcDirection(aspect, toPoint, point, pointSpeed){
+		
+		if( (point - toPoint) > 0 ){
+			
+			if((point - toPoint) > astrology.utils.radiansToDegree( Math.PI)){
+				point = (point + aspect) % astrology.utils.radiansToDegree( 2 * Math.PI);
+			}else{
+				toPoint = (toPoint + aspect) % astrology.utils.radiansToDegree( 2 * Math.PI);
+			}			
+		}else{
+			
+			if((toPoint - point) > astrology.utils.radiansToDegree( Math.PI)){
+				toPoint = (toPoint + aspect) % astrology.utils.radiansToDegree( 2 * Math.PI);
+			}else{
+				point = (point + aspect) % astrology.utils.radiansToDegree( 2 * Math.PI);
+			}										
+		}
+		
+		var _point = point;
+		var _toPoint = toPoint;
+		
+		var difference = _point - _toPoint;
+		
+		if( Math.abs( difference ) > astrology.utils.radiansToDegree( Math.PI)){			
+			_point = toPoint;
+			_toPoint = point;
+		}
+							
+		return (_point - _toPoint < 0) ? -1 : 1;				
 	}
 		
 }( window.astrology = window.astrology || {}));
