51ac6426691475ca3617e875301a4b5b9e6a626b
Arthur Fücher <arthurfucher@gmail.com>
2022-04-22 22:44:55 -0300

    migrate core test to jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  collectCoverageFrom: ['./project/src/**/*.{ts,js}']
+  collectCoverageFrom: ['./project/src/**/*.{ts,js}'],
+  testEnvironment: 'jsdom'
 };
